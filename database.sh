2e9fe8746f14e55219eccb913d058dbb1c3804ad
Taghreed Muhammed <taghreedmuhammed121@gmail.com>
2024-01-09 23:46:14 +0200

    first

--- database.sh
+++ database.sh
@@ -1,13 +1,13 @@
 select choice in Create_Database List_Database Drop_Database Connect_Database 
 do 
 case $REPLY in
-1 ) ./createDb
+1 ) ./createDb.sh
 ;;
-2 ) ./listDb
+2 ) ./listDb.sh
 ;;
-3 ) ./dropDb
+3 ) ./dropDb.sh
 ;;
-4 ) ./connectDb
+4 ) ./connectDb.sh
 ;;
 * ) 
 echo "Error"
